dc68ac80d8710163beea31715ad4be65f7ded5bb
apluth <Apluth@crimson.ua.edu>
2017-01-18 16:28:20 -0600

    Unity tutorial

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        WinText.text = "";
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 15)
+        {
+            WinText.text = "You Win!!!";
+        }
+    }
+
+}
